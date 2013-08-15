69a92fc76510b0df366efcb5b6d1ff3d0bdc892d
ArdentCat <lekha_dipika@yahoo.co.in>
2013-08-15 16:51:12 +0530

    finding error code generalized and error matrix creation code updated

--- src/spellchecker/ErrorMatrices.java
+++ src/spellchecker/ErrorMatrices.java
@@ -14,7 +14,16 @@ public class ErrorMatrices {
 	int dMatrix[][] = new int [27][27]; //deletion counts
 	int charCount[] = new int [27]; //count of occurence of a given character
 	
+	static final int NO_ERROR 		= 	0;
+	static final int INSERTION 		= 	1;
+	static final int DELETION 		= 	2;
+	static final int SUBSTITUTION 	= 	3;
+	static final int TRANSPOSITION 	= 	4;
+	static final int UNKNOWN_ERROR 	= 	5;
+	
+	
 	//	charIndex assigns numbers for a to z
+	
 	HashMap<Character, Integer> charIndex = new HashMap<Character, Integer>();
 	
 	public ErrorMatrices()
@@ -25,133 +34,191 @@ public class ErrorMatrices {
 	}
 	
 	/*
+	 * Input: Wrong word and correct word
+	 * Output: c[0] contains 1,2,3,4 depending on type of error i,d,s or x 
+	 * 		   c[0] contains 0 for no error
+	 * 		   c[0] contains 5 for impossibility of single error i.e. there may be more than one transformation etc not a single insert/delete/transpose/substitution 
+	 * c[1] contains index of first char
+	 * c[2] contains index if second char
+	 * eg for insertion c[1] contains (index of) 't' c[2] contains 'e' if 'e' is inserted after 't'
+	 * similarly for deletion
+	 * for substitution c[1] is replaced by c[2]
+	 * for xposition c[1]c[2] was the correct order but it was exchanged in wrong word
+	 * for no error or unknown error c[1] and c[2] are invalid
+	 * */
+	
+	public int[] findError(String wrong, String correct)
+	{
+		int answer[] = new int[3];
+		if(wrong.equals(correct)) //no error
+			answer[0] = NO_ERROR;
+		else if( ( wrong.length() > ( correct.length() + 1 ) ) || ( wrong.length() < ( correct.length() -1  ) ) ) //cannot be a single error
+			answer[0] = UNKNOWN_ERROR;
+		else //difference in length has to be atmost 1 can be insert/delete/substitute/transpose/ or 'e' impossible
+		{
+			
+			int lengthWrong = wrong.length();
+			int lengthCorrect = correct.length();
+			
+			if(lengthWrong > lengthCorrect)//insert error possible
+			{
+				//compare chars till they match
+				int i;
+				for( i= 0 ; i < lengthCorrect ; i++)
+				{
+					if(wrong.charAt(i) == correct.charAt((i)))
+					{	/*Do nothing simply advance pointers */ }
+					else
+					{	break;	}
+				}
+				//i now points to index where corresponding chars differ
+				if(i == lengthCorrect )//insertion on last char
+				{
+					answer[0] = INSERTION;
+					answer[1] = (i>0) ? charIndex.get( correct.charAt(i-1) ) : 0;
+					answer[2] = charIndex.get( wrong.charAt(i) );
+				}
+				else if( correct.substring( i ).equals( wrong.substring( i + 1 ) ) ) //insertion before last char
+				{
+					answer[0] = INSERTION;
+					answer[1] = (i>0) ? charIndex.get( correct.charAt(i-1) ) : 0;
+					answer[2] = charIndex.get( wrong.charAt(i) );
+				}
+				else //not a single insertion error
+				{
+					answer[0] = UNKNOWN_ERROR;
+				}
+				
+			}
+			else if(lengthWrong < lengthCorrect) //delete error possible
+			{
+				//compare chars till they match
+				int i;
+				for( i= 0 ; i < lengthWrong ; i++)
+				{
+					if(wrong.charAt(i) == correct.charAt(i))
+					{	/*do nothing*/	}
+					else
+					{	break;		}
+				}
+				//i has index where corresponding chars differ
+				if( i == lengthWrong ) //deletion at end
+				{
+					answer[0] = DELETION;
+					answer[1] = (i>0) ? charIndex.get( correct.charAt(i-1) ) : 0;
+					answer[2] = charIndex.get( correct.charAt(i) );
+				}
+				else if ( correct.substring( i + 1 ).equals( wrong.substring( i ) ) ) //deletion before last char
+				{
+					answer[0] = DELETION;
+					answer[1] = (i>0) ? charIndex.get( correct.charAt(i-1) ) : 0;
+					answer[2] = charIndex.get( correct.charAt(i) );
+				}
+				else
+				{
+					answer[0] = UNKNOWN_ERROR;
+				}
+				
+			}
+			else //length equal so substitution or transposition possible
+			{
+				//match corresponding chars
+				int i;
+				for ( i = 0 ; i < lengthCorrect ; i++ )
+				{
+					if(wrong.charAt(i) == correct.charAt(i))
+					{ /*do nothing just advance pointer*/	}
+					else
+					{		break;		}
+				}
+				//i can never be lengthCorrect(since we've already tested for no error in beginning)
+				
+				if (i == (lengthCorrect-1))//last letter doesn't match thus it is a substitution
+				{
+					answer[0] = SUBSTITUTION;
+					answer[1] = charIndex.get( correct.charAt(i) );
+					answer[2] = charIndex.get( wrong.charAt(i) );
+				}
+				else//either substitution or transpose
+				{
+					if( wrong.substring( i + 1 ).equals( correct.substring( i + 1 ) ) )//substitution
+					{
+						answer[0] = SUBSTITUTION;
+						answer[1] = charIndex.get( correct.charAt(i) );
+						answer[2] = charIndex.get( wrong.charAt(i) );
+					}
+					else
+					{
+						if( correct.charAt(i) == wrong.charAt(i+1) && correct.charAt(i+1) == wrong.charAt(i) )
+						{
+							if( (i+2) == lengthCorrect ) //transposition at the end
+							{
+								answer[0] = TRANSPOSITION;
+								answer[1] = charIndex.get(correct.charAt(i));
+								answer[2] = charIndex.get(correct.charAt(i+1));
+							}
+							else if ( correct.substring( i + 2 ).equals( wrong.substring( i + 2 ) ) )
+							{
+								answer[0] = TRANSPOSITION;
+								answer[1] = charIndex.get(correct.charAt(i));
+								answer[2] = charIndex.get(correct.charAt(i+1));
+							}
+							else
+							{
+								answer[0] = UNKNOWN_ERROR;
+							}
+						}
+						else
+						{
+							answer[0] = UNKNOWN_ERROR;
+						}
+					}
+				}			
+			}		
+		}
+		return answer;
+	}
+	
+	/*
 	 * function takes 2 words as input, a wrong word and its corresponding correct word
 	 * it then finds type of error, insert, delete, substitution or transpose
 	 * it then updates appropriate matrix with error count values*/
-	
-	
 	public void updateMatrices(String wrong, String correct)
 	{
-		int lengthWrong = wrong.length();
-		int lengthCorrect = correct.length();
+		int result[] = findError(wrong, correct);
 		
-		//System.out.println("wrong word: "+wrong+"\tlength: "+lengthWrong);
-		//System.out.println("correct word: "+correct+"\tlength: "+lengthCorrect);
-
-		charCount[0]++;
-		for(int i = 0 ; i < correct.length() ; i++)
+		switch(result[0])
 		{
-			//update charCount of ith character
-			charCount[charIndex.get(correct.charAt(i))] ++;
-		}
 		
-		//now we make a single error assumption and first detect which type of error has occured
-		//1-insert
-		//2-delete
-		//3-substitution
-		//4-transpose
+		case NO_ERROR: 
 		
-		if(lengthWrong > lengthCorrect)//insert error
-		{
-			//System.out.println("insert");
+			//do nothing
+			break;
+		
+		case INSERTION:
 			
-			//the following loop compares chars of correct and wrong string
-			//till corresponding chars are found same, no action is performed
-			//the first point of difference gives us the inserted char
-			int i;
-			for( i= 0 ; i < lengthCorrect ; i++)
-			{
-				if(wrong.charAt(i) == correct.charAt((i)))
-				{
-					//Do nothing simply advance pointers
-				}
-				else
-				{
-					break;
-				}
-			}
-			int previousCharIndex = (i>0) ? charIndex.get(correct.charAt(i-1)):0;
-			int insertedCharIndex = charIndex.get(wrong.charAt(i));
-			iMatrix[previousCharIndex][insertedCharIndex] ++; //update insert matrix count
-			//System.out.println("inserted "+insertedCharIndex+" after "+previousCharIndex);
-			//System.out.println("inserted char: "+wrong.charAt(i));
-		}
-		else if(lengthWrong < lengthCorrect) //delete error
-		{
-			//System.out.println("delete");
+			iMatrix[ result[1] ][ result[2] ] ++ ; //update insertion matrix
+			break;
+		
+		case DELETION:
+		
+			dMatrix[ result[1] ][ result[2] ] ++ ; //update deletion matrix
+			break;
+		
+		case SUBSTITUTION:
 			
+			sMatrix[ result[1] ][ result[2] ] ++ ; //update substitution matrix
+			break;
+		
+		case TRANSPOSITION:
 			
-			int i;
-			for( i= 0 ; i < lengthWrong ; i++)
-			{
-				if(wrong.charAt(i) == correct.charAt(i))
-				{
-					//do nothing
-				}
-				else
-				{
-					break;
-				}
-			}
-			int previousCharIndex = (i>0) ? charIndex.get(correct.charAt(i-1)):0;
-			int deletedCharIndex = charIndex.get(correct.charAt(i));
-			dMatrix [previousCharIndex][deletedCharIndex] ++; //update deletion matrix
-			//System.out.println("deleted "+deletedCharIndex+" after "+previousCharIndex);
-			//System.out.println("char deleted: "+correct.charAt(i));
-		}
-		else //substitution or transposition
-		{
-			int i;
-			for ( i = 0 ; i < lengthCorrect ; i++ )
-			{
-				if(wrong.charAt(i) == correct.charAt(i))
-				{
-					//do nothing
-				}
-				else
-				{
-					break;
-				}
-			}
+			xMatrix[ result[1] ][ result[2] ] ++ ; //update transposition matrix
+			break;
+		
+		case UNKNOWN_ERROR: 
 			
-			//System.out.println(i);
-			
-			if(i == lengthCorrect)//no error found
-			{
-				//do nothing wrt error matrices
-				//System.out.println("No error");
-			}
-			
-			else if (i == (lengthCorrect-1))//last letter doesn't match thus it is a substitution
-			{
-				int originalCharIndex = charIndex.get(correct.charAt(i));
-				int substitutedCharIndex = charIndex.get(wrong.charAt(i));
-				sMatrix [originalCharIndex][substitutedCharIndex] ++; //update substitution matrix count
-				
-				//System.out.println("substitution: "+originalCharIndex+" changed to "+substitutedCharIndex);
-			}
-			else//either substitution or transpose
-			{
-				if(wrong.substring(i+1).equals(correct.substring(i+1)))
-				{
-					int originalCharIndex = charIndex.get(correct.charAt(i));
-					int substitutedCharIndex = charIndex.get(wrong.charAt(i));
-					sMatrix [originalCharIndex][substitutedCharIndex] ++; //update substitution matrix count
-					
-					//System.out.println("substitution: "+originalCharIndex+" changed to "+substitutedCharIndex);
-				}
-				else
-				{
-					if(correct.charAt(i) == wrong.charAt(i+1) && correct.charAt(i+1) == wrong.charAt(i))
-					{
-						int firstCharIndex = charIndex.get(correct.charAt(i));
-						int secondCharIndex = charIndex.get(correct.charAt(i+1));
-						
-						xMatrix [firstCharIndex][secondCharIndex] ++;
-						//System.out.println("transpose: "+firstCharIndex+" exchanged with "+secondCharIndex);
-					}
-				}
-			}
+			//do nothing
+			break;
 			
 		}
 	}
@@ -200,6 +267,7 @@ public class ErrorMatrices {
 			System.out.println(i+" : "+charCount[i]);
 		}
 	}
+	
 	public void printIMatrix()
 	{
 		System.out.println("\n---- I matrix ----\n");
@@ -275,26 +343,31 @@ public class ErrorMatrices {
 		em.updateMatrices("abc", "bc"); //insertion at start
 		em.updateMatrices("abc", "bc"); //insertion at start
 		em.updateMatrices("abcd", "abc");//insertion at end
-		//em.updateMatrices("abdc", "abc"); //insertion in middle
+		em.updateMatrices("abdc", "abc"); //insertion in middle
 		System.out.println("P(d/c) "+em.getIProbability(3, 4));
 		em.printIMatrix();
 		
-		//em.updateMatrices("bc", "abc"); //deletion at start
-		//em.updateMatrices("abc", "abcd");//deletion at end
-		//em.updateMatrices("abc", "abdc"); //deletion in middle
+		em.updateMatrices("bc", "abc"); //deletion at start
+		em.updateMatrices("abc", "abcd");//deletion at end
+		em.updateMatrices("abc", "abdc"); //deletion in middle
+		em.printDMatrix();
 		
-		//em.printDMatrix();
+		em.updateMatrices("abc", "acb"); //xposition at end
+		em.updateMatrices("abc", "bac"); //xpsn at start
+		em.updateMatrices("abcd", "acbd");//xpsn in middle
+		em.printXMatrix();
 		
-		//em.updateMatrices("abc", "acb"); //xposition at end
-		//em.updateMatrices("abc", "bac"); //xpsn at start
-		//em.updateMatrices("abcd", "acbd");//xpsn in middle
+		em.updateMatrices("abcd", "abce"); //subs at end
+		em.updateMatrices("abcd", "ebcd"); //subs at start
+		em.updateMatrices("abcd", "abed"); //subs in middle
+		em.printSMatrix();
 		
-		//em.printXMatrix();
-		
-		//em.updateMatrices("abcd", "abce"); //subs at end
-		//em.updateMatrices("abcd", "ebcd"); //subs at start
-		//em.updateMatrices("abcd", "abed"); //subs in middle
-		//em.printSMatrix();
+	/*
+		int ch[] = em.findError("lekha", "lekha");
+		System.out.println("Error type: "+ch[0]);
+		System.out.println("Char1: "+ch[1]);
+		System.out.println("Char2: "+ch[2]);
+		*/
 		
 	}
 
--- src/test.java
+++ src/test.java
@@ -5,8 +5,10 @@ public class test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String s="123";
-		System.out.println(s.matches("[a-z]*"));
+		String s="abxc";
+		String s2="abdc";
+		System.out.println("-"+s.substring(5).equals(s2.substring(4))+"-");
+				
 
 	}
 
